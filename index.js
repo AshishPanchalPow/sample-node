07e530a5c2f7abc29c596ab49f29db1e5821e8c9
ashishpanchalpow <sunnypanchal1991@gmail.com>
2023-02-27 14:07:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var webhook_res=""
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.post('/events/rec',function (request, response) {
-//  console.log(request.body);
+ console.log(request.body);
  if('data' in request.body[0]){
    if('validationCode' in request.body[0].data) {
      webhook_res = {'validationResponse': request.body[0].data.validationCode}
@@ -14,10 +15,10 @@ app.post('/events/rec',function (request, response) {
      response.send(webhook_res);
    }
     if(request.body[0].data.api=='PutBlob'){
-       console.log('&gt;&gt; Blob uploaded - %s', request.body[0].data.url)
+       console.log('>> Blob uploaded - %s', request.body[0].data)
      }
      if(request.body[0].data.api=='DeleteBlob'){
-       console.log('&gt;&gt; Blob deleted - %s', request.body[0].data.url)
+       console.log('>> Blob deleted - %s', request.body[0].data)
      }
      response.send();
      response.end();
@@ -26,8 +27,9 @@ app.post('/events/rec',function (request, response) {
 
 app.get('/', function (req, res){
     res.status(200).send({msg: "Hola"});
-})
-var server = app.listen(8000, function () {
- console.log('App listening on port %s', server.address().port);
+});
+
+var server = app.listen(port, function () {
+ console.log('App listening on port %s', port);
  console.log('Press Ctrl+C to quit.');
 });
